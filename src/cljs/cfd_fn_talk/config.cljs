3466131eae99efb42fb89788ec09c9709ec82caa
dviramontes <dviramontes@gmail.com>
2016-09-10 01:25:48 -0600

    adds firebase details, generate id based on col row position for consistency, adds card click handler function

--- src/cljs/cfd_fn_talk/config.cljs
+++ src/cljs/cfd_fn_talk/config.cljs
@@ -1,4 +1,22 @@
-(ns cfd-fn-talk.config)
+(ns cfd-fn-talk.config
+  (:require [cljsjs.firebase]))
 
 (def debug?
   ^boolean js/goog.DEBUG)
+
+(defonce firebase-app-init
+         {:apiKey        "I9YgeqfDYhR8N8AuhFhNBzVF4ZNrngVjcHZVthag"
+          :authDomain    "cdf-fn-talk.firebaseapp.com"
+          :databaseURL   "https://cdf-fn-talk.firebaseio.com"
+          :storageBucket "cdf-fn-talk.appspot.com"})
+
+(defonce firebase-app
+         (.initializeApp js/firebase (clj->js firebase-app-init)))
+
+(defonce firebase-db-ref
+         (-> firebase-app .database .ref))
+
+(defn ref-for-path
+  "Returns a Firebase ref for the node at the given path string relative to firebase-db-ref."
+  [rel-path]
+  (.child firebase-db-ref rel-path))
--- src/cljs/cfd_fn_talk/views.cljs
+++ src/cljs/cfd_fn_talk/views.cljs
@@ -1,27 +1,29 @@
 (ns cfd-fn-talk.views
   (:require [re-frame.core :as re-frame]
             [cfd-fn-talk.db :as db]
-            [cljsjs.firebase]))
+            [cljsjs.firebase]
+            [cfd-fn-talk.config :refer [ref-for-path firebase-db-ref]]))
+
+(defn card-opts [title index]
+  (let [name (str (name title) "-" index)]
+    {:id       name
+     :on-click (fn [e]
+                 (let [path (ref-for-path (str "jeopardy/" name))]
+                   (.set path
+                         (clj->js {:taken false})))
+                 (prn (-> e .-target .-id)))}))
+
+(defn card-contents [title index amount]
+  [:p.notification.is-warning.purple
+   (card-opts title index) amount])
 
 (defn card [title]
-  [:div.column
-   [:p.notification.is-warning title]
-   [:p.notification.is-warning.purple
-    {:id       (gensym (str (name title) "-"))
-     :on-click (fn [e] (prn (-> e .-target .-id)))}
-    "$200"]
-   [:p.notification.is-warning.purple
-    {:id       (gensym (str (name title) "-"))
-     :on-click (fn [e] (prn (-> e .-target .-id)))}
-    "$400"]
-   [:p.notification.is-warning.purple
-    {:id       (gensym (str (name title) "-"))
-     :on-click (fn [e] (prn (-> e .-target .-id)))}
-    "$800"]
-   [:p.notification.is-warning.purple
-    {:id       (gensym (str (name title) "-"))
-     :on-click (fn [e] (prn (-> e .-target .-id)))}
-    "$1000"]])
+  (let [amounts ["$200" "$400" "$800" "$1000"]]
+    [:div.column
+     [:p.notification.is-warning title]
+     (map-indexed (fn [index amount]
+                    ^{:key index}
+                    [card-contents title index amount]) amounts)]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
